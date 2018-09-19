cf381c1fb479c50cfbda24b703e12fb2e1a2b966
Joshua Rogers <Joshua.Rogers@iam.local>
2018-09-19 15:14:34 -0500

    WIP PlayerCharacter
    Added and working on PlayerCharacter script

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Rawr");  //How to print to the console in Unity
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+}
