30630ca16fd8de71d19366b2489b664d8ca40454
Joshua Rogers <Joshua.Rogers@iam.local>
2018-10-03 15:17:49 -0500

    Physics Materials, Jump, and Movement Change
    -Changed Player Movement code in Player Character Script
    -Added Jump Function
    -Added Player, Slippery, and Slow Physics Materials

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -6,20 +6,33 @@ public class PlayerCharacter : MonoBehaviour
 {
     [SerializeField]
     private int lives = 3;
-    [SerializeField]
 
+    [SerializeField]
     private string name = "Mario";
 
     [SerializeField]
-    private float jumpHeight=5, speed=5;
+    private float jumpHeight = 5;
+    
+    [SerializeField]
+    private float accelerationForce = 5;
+
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private float jumpForce = 10;
 
     private bool hasKey;
 
     private bool isOnGround;
 
+    [SerializeField]
     private Rigidbody2D rigidbody2DInstance;
 
     private float horizontalInput;
+
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
 	// Use this for initialization
 	void Start ()
     {
@@ -28,7 +41,7 @@ public class PlayerCharacter : MonoBehaviour
         string pizza = "yum";
         Debug.Log(pizza);
 
-        rigidbody2DInstance.gravityScale = 5;
+        //rigidbody2DInstance.gravityScale = 5;
 
         /*if(hasKey)
         {
@@ -46,21 +59,35 @@ public class PlayerCharacter : MonoBehaviour
 	private void Update ()
     {
         //transform.Translate(0, -.01f, 0); don't use because of physics
-        GetInput();
-        Move();
-
+        //GetInput();
+        horizontalInput = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigidbody2DInstance.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        
     }
 
-    private void GetInput()
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    /*private void GetInput()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-    }
+    }*/
     private void Move()
     {
-        rigidbody2DInstance.velocity = new Vector2(horizontalInput, 0);
+        //rigidbody2DInstance.velocity = new Vector2(horizontalInput, 0);
+        rigidbody2DInstance.AddForce(Vector2.right * horizontalInput * accelerationForce);
+        Vector2 clampedVelocity = rigidbody2DInstance.velocity;
+        clampedVelocity.x = Mathf.Clamp(rigidbody2DInstance.velocity.x, -maxSpeed, maxSpeed);
+        rigidbody2DInstance.velocity = clampedVelocity;
     }
-    private void MoveRight()
+   /* private void MoveRight()
     {
         rigidbody2DInstance.velocity = new Vector2(5, 0);
-    }
+    }*/
+
 }
