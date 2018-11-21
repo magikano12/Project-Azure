0c5b90cb3b10b78b929060ed0a0ad8f8089b193a
Joshua Rogers <Joshua.Rogers@iam.local>
2018-11-21 13:36:08 -0600

    More Checkpoint Stuff
    -Added rotation to checkpoint along y-axis
    -Made visual cues indicating when a checkpoint is crossed
          -Checkpoint has an inactive and
           active state. Activating a
           checkpoint increases its rotation
           speed, scale, and color

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -128,6 +128,11 @@ public class PlayerCharacter : MonoBehaviour
     }
     public void SetCurrentCheckpoint(Checkpoint newCurrentCheckpoint)
     {
+        if (currentCheckpoint != null)
+        {
+            currentCheckpoint.SetIsActivated(false);
+        }
         currentCheckpoint = newCurrentCheckpoint;
+        currentCheckpoint.SetIsActivated(true);
     }
 }
