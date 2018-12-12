f2ae5c931d9281280a38569212b085a1d1c280dc
Joshua Rogers <Joshua.Rogers@iam.local>
2018-12-12 15:15:19 -0600

    Re-adding scripts for code review

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    private int coinCount;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            audioSource.Play();
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            Destroy(gameObject, audioSource.clip.length);
+        }
+    }
+}
---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Hazard : MonoBehaviour
+{
+
+    
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player has entered the hazard");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+            player.Die();  
+        }
+
+        else
+        {
+            Debug.Log("Something other than the player has entered the hazard");
+        }
+        
+    }
+
+}
